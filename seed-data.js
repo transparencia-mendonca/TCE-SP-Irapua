5b4802f504597a2d3f19318256e8732392616bcd
transparencia-mendonca <miguel.angelogarcia@gmail.com>
2026-09-20 23:16:26 -0300

    Add files via upload

---
+++ seed-data.js
@@ -0,0 +1,2 @@
+window.SEED_DATA={_bundleVersion:"irapua-baseline-2026-09-20",exercicios:[],despesas:[],receitas:[],apiUpdates:{},autoCheck:{}};
+window.TCESP_PERMANENT_BASELINE={manifest:"baseline-manifest.json",version:"irapua-baseline-2026-09-20"};
